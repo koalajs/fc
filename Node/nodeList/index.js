f651fb34ac3d9f20641950f0e4cd5992fc6b79e5
Jon.Xia <xiajia@vip.qq.com>
2019-01-11 17:05:07 +0800

    改用新的ts-orm

--- Node/nodeList/index.js
+++ Node/nodeList/index.js
@@ -1,22 +1,26 @@
 const config = require('./config')
 const utils = require('@koala.js/fc.utils')
-const orm = require('@koala.js/fc.utils/ts-orm')
+const ts = require('@koala.js/ts-orm')
 const TableStore = require('tablestore')
 
 const handler = async (req, rep, ctx) => {
-  const res = await orm.getRange
+  const res = await ts.orm
     .init(config.table)
     .table('Auth')
-    .start({
+    .direction(TableStore.Direction.FORWARD)
+    .startKeys({
       tk: 'up',
       id: TableStore.INF_MIN
     })
-    .end({
+    .endKeys({
       tk: 'up',
       id: TableStore.INF_MAX
     })
-    .find()
-  utils.httpSend(rep, res)
+    .limit(100)
+    .getRange()
+  // 对数据做处理，形成扁平化列表
+  const data = ts.formatRange(res) 
+  utils.httpSend(rep, data.rows)
 }
 
 const initializer = (context, callback) => {
